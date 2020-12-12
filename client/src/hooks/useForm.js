8f31765d3cef7776617a7d365dc19a45e6fa8c11
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2020-12-12 12:39:57 -0500

    added useForm hook to control state values with a custom hook

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,13 @@
 // write your custom hook here to control your checkout form
+import React, {useState} from 'react';
+
+export default function useForm(initialValue) {
+    const [formValues, setFormValues] = useState(initialValue);
+
+    const handleChanges = (name, value) => {
+        setFormValues({ ...formValues, [name]: value });
+      };
+      
+    return [formValues, handleChanges];
+
+}
